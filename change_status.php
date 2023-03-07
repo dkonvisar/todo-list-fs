8caa42abeb1547d1e33a2e2fc25a1804e5b265e7
denis <denis.konvisar@gmail.com>
2023-03-07 12:06:37 +0200

    init

---
+++ change_status.php
@@ -0,0 +1,11 @@
+<?php
+
+$file_name = 'todo.json';
+$json = file_get_contents($file_name);
+$jsonArray = json_decode($json, true);
+
+$todo_name = htmlspecialchars($_POST['todo_name']);
+$jsonArray[$todo_name]['completed'] = !$jsonArray[$todo_name]['completed'];
+
+file_put_contents($file_name, json_encode($jsonArray, JSON_PRETTY_PRINT));
+header('Location: index.php');
---
+++ new_todo.php
@@ -0,0 +1,19 @@
+<?php
+
+$todo_name = $_POST['todo_name'] ?? '';
+$todo_name = htmlspecialchars(trim($todo_name));
+
+if ($todo_name) {
+    if (file_exists('todo.json')) {
+        $json = file_get_contents('todo.json');
+        $jsonArray = json_decode($json, true);
+    } else {
+        $jsonArray = [];
+    }
+
+    $jsonArray[$todo_name] = ['completed' => false];
+    file_put_contents('todo.json', json_encode($jsonArray, JSON_PRETTY_PRINT));
+
+}
+
+header('Location: index.php');
